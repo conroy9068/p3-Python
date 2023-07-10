d1b9ce8df604e459946bcb48eabc0d0cda138da9
David <david@trave.io>
2023-07-10 21:00:28 +0100

    fix: continue statement added to stop app from crashing after invalid menu entry option

--- run.py
+++ run.py
@@ -306,6 +306,7 @@ if __name__ == "__main__":
 
         else:
             print("Wrong Choice!!!! Try Again\n")
+            continue
 
         # Stores the winner in a single game of Tic Tac Toe
         winner = single_game(options[choice-1])
